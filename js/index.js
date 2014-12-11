a42f884180f0b1e767bb149b750e53fae5f5fe15
BO LIAO <liao0015@algonquinlive.com>
2014-12-10 19:47:35 -0500

    final commit

--- js/index.js
+++ js/index.js
@@ -1,51 +1,77 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+var myList = [];
 
-        console.log('Received Event: ' + id);
-    }
-};
 
-app.initialize();
+//document.addEventListener("DOMContentLoaded", onDeviceReady, false);
+document.addEventListener('deviceready', onDeviceReady, false);
+
+	function onDeviceReady () {
+	console.log("page loaded");
+  	if(localStorage.getItem("grocery-liao0015")){
+    	myList = JSON.parse(localStorage.getItem("grocery-liao0015"));
+  	}
+		showList();
+		var btnAdd = document.querySelector("#btnAdd");
+		btnAdd.addEventListener("click", btnAdd_click);    			
+	}
+
+	function btnAdd_click (ev) {
+		ev.preventDefault();
+    	var newItem = document.querySelector("#item").value;
+		if(newItem.trim() != ""){
+			myList.push( newItem );
+			localStorage.setItem("grocery-liao0015", JSON.stringify(myList) );
+		} else {
+			alert("Nothing to add!");
+		}
+    		showList();
+    		return false;
+	}
+
+	function removeItem(ev){
+		var txt = ev.currentTarget.parentNode.firstChild.nodeValue;
+		if (ev.currentTarget.previousSibling.firstChild.checked == true) {
+			for(var i=0;i<myList.length;i++){
+				if(myList[i] == txt){
+				myList.splice(i, 1);
+				} 
+  			}
+			localStorage.setItem("grocery-liao0015", JSON.stringify(myList) );
+			showList();
+		}else {
+			alert ("You must select before you can delete");
+		}
+	}
+
+	function showList(){
+		var ul = document.querySelector("ul");
+  		ul.innerHTML = "";
+		
+  		for(var i=0;i<myList.length;i++){
+			//checkbox created
+			var form = document.createElement("form");
+			var checkBox = document.createElement("input");
+			checkBox.setAttribute ("type","checkbox");
+			checkBox.setAttribute ("class","mycheck");
+			form.appendChild(checkBox);
+			
+			//delete button created
+			var deleteBtn = document.createElement("button");
+			deleteBtn.setAttribute ("class","delbtn");
+			var del = document.createTextNode("Delete");
+			deleteBtn.appendChild(del);
+			
+			//item from local storage
+    		var li = document.createElement("li");
+    		li.innerHTML = myList[i];
+			li.appendChild(form);
+			li.appendChild(deleteBtn);
+			ul.appendChild(li);
+			
+			deleteBtn.addEventListener("click", removeItem);
+  		}
+   }
+
+
+
+
